7d59dc0f9390355b6f230e910e6cd16e5632653d
Florian Klein <fklein@users.noreply.github.com>
2024-01-22 17:13:42 +0100

    Add a user entity

---
+++ src/main/java/dev/fklein/demoservice/entities/User.java
@@ -0,0 +1,117 @@
+package dev.fklein.demoservice.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity  //@Entity(name = "User")
+@Table(name = "USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "FIRST_NAME", length = 50, nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", length = 50, nullable = false)
+    private String lastName;
+
+    @Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "ROLE", length = 50, nullable = false)
+    private String role;
+
+    @Column(name = "SSN", length = 50, nullable = false, unique = true)
+    private String ssn;
+
+    // JPA requires a no-args constructor + getters/setters for all fields
+    public User() {
+    }
+
+    public User(String userName, String firstName, String lastName, String email, String role, String ssn) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.ssn = ssn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", ssn='" + ssn + '\'' +
+                '}';
+    }
+}
