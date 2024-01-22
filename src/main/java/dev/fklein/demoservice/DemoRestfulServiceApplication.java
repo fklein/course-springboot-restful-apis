3b8193b306b53f6130c3363fa75f2d0ce7a22be5
Florian Klein <fklein@users.noreply.github.com>
2024-01-22 14:39:31 +0100

    Import project setup from Spring Initializr

---
+++ src/main/java/dev/fklein/demoservice/DemoRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.fklein.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestfulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRestfulServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fklein/demoservice/DemoRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fklein.demoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
