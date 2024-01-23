22f1728d1f7df8eaf13ddb313db23e1da0e52083
Florian Klein <fklein@users.noreply.github.com>
2024-01-23 17:08:24 +0100

    Create a user service

---
+++ src/main/java/dev/fklein/demoservice/services/UserService.java
@@ -0,0 +1,9 @@
+package dev.fklein.demoservice.services;
+
+import dev.fklein.demoservice.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+}
---
+++ src/main/java/dev/fklein/demoservice/services/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package dev.fklein.demoservice.services;
+
+import dev.fklein.demoservice.entities.User;
+import dev.fklein.demoservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
