c13ec5cd2c00e29a8d4db6e16ad2b51613248d5c
Florian Klein <fklein@users.noreply.github.com>
2024-01-26 13:36:36 +0100

    Do I18N for the hello world controller

--- src/main/java/dev/fklein/demoservice/DemoRestfulServiceApplication.java
+++ src/main/java/dev/fklein/demoservice/DemoRestfulServiceApplication.java
@@ -2,11 +2,31 @@ package dev.fklein.demoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class DemoRestfulServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoRestfulServiceApplication.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver locale = new AcceptHeaderLocaleResolver();
+        locale.setDefaultLocale(Locale.US);
+        return locale;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("internationalization/messages");
+        return messageSource;
     }
 }
--- src/main/java/dev/fklein/demoservice/web/HelloWorldController.java
+++ src/main/java/dev/fklein/demoservice/web/HelloWorldController.java
@@ -1,13 +1,26 @@
 package dev.fklein.demoservice.web;
 
 import dev.fklein.demoservice.dto.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private final ResourceBundleMessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(ResourceBundleMessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     // @GetMapping("/helloworld")
     @RequestMapping(method= RequestMethod.GET, path="/helloworld")
@@ -18,5 +31,21 @@ public class HelloWorldController {
     @GetMapping("/userdetails")
     public UserDetails getUserDetails() {
         return new UserDetails("Max", "Mustermann", "München");
+    }
+
+    @GetMapping("/helloworld-int")
+    public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+        Locale l;
+        try {
+            l = new Locale(locale);
+        } catch (Exception e) {
+            l = Locale.US;
+        }
+        return messageSource.getMessage("label.hello", null, l);
+    }
+
+    @GetMapping("/helloworld-int2")
+    public String getMessagesInI18NFormat2() {
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
     }
 }
