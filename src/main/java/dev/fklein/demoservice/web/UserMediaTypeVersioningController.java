7e43d8ebc7f9597f0b3aa03e674c0c8324456335
Florian Klein <fklein@users.noreply.github.com>
2024-01-30 13:33:43 +0100

    Implement versioning via media type

---
+++ src/main/java/dev/fklein/demoservice/web/UserMediaTypeVersioningController.java
@@ -0,0 +1,53 @@
+package dev.fklein.demoservice.web;
+
+import dev.fklein.demoservice.dto.UserDtoV1;
+import dev.fklein.demoservice.dto.UserDtoV2;
+import dev.fklein.demoservice.entities.User;
+import dev.fklein.demoservice.exceptions.UserNotFoundException;
+import dev.fklein.demoservice.services.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/versioning/mediatype/users")
+public class UserMediaTypeVersioningController {
+
+    private final UserService userService;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public UserMediaTypeVersioningController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/vnd.fklein.app-v1+json")
+    public UserDtoV1 getUserByIdV1(@PathVariable("id") Long id) {
+        try {
+            User user = userService.getUserById(id);
+            UserDtoV1 userDto = modelMapper.map(user, UserDtoV1.class);
+            return userDto;
+        } catch (UserNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/vnd.fklein.app-v2+json")
+    public UserDtoV2 getUserByIdV2(@PathVariable("id") Long id) {
+        try {
+            User user = userService.getUserById(id);
+            UserDtoV2 userDto = modelMapper.map(user, UserDtoV2.class);
+            return userDto;
+        } catch (UserNotFoundException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        }
+    }
+
+}
